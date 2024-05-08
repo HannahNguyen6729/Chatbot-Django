23492c00610fdc35fea617e5dd87b59c955074aa
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-05-08 11:02:17 +0300

    create chatbot route and add view for that route

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,8 @@
+
 from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
+def chatbot(request):
+    return render(request, 'chatbot.html')
+   # return HttpResponse('hi')
